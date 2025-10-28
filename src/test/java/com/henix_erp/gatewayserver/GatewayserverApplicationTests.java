bd5c33bc5f670a268d60e7703aae71349ccd5ea0
Hitesh <hitesh.ahirwar@techvalens.com>
2025-10-28 13:55:50 +0530

    Initial commit

---
+++ src/test/java/com/henix_erp/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henix_erp.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
